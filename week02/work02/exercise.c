37381b633c2d6dc0a98d9afe0de924bdf74b05ae
FARHAN NURDIATAMA - 1506721825 <farhan.nurdiatama@badak.cs.ui.ac.id>
2017-02-21 20:46:19 +0700

    this lab is exhausting

---
+++ week02/work02/exercise.c
@@ -0,0 +1,19 @@
+/*
+* Name: Farhan Nurdiatama
+* NPM: 1506721825
+* Class: OSB
+* Comment: Heyo
+*/
+#define LOOP 3 //define me!
+#include <stdio.h>
+void main() {
+	 int input = 1; //define me!
+	int total = 0;
+	int i;
+ 	// TO DO implement me!!
+	for (i = 0; i < LOOP; i++){
+		total += input;
+	}
+	printf( "%d times %d equals %d\n", input, LOOP, total);
+}
+
